7634967c5b9b269da72db7b413bc16ea1b11176e
mikesvr6 <michaeloceallachain@gmail.com>
2024-07-11 16:31:22 +0000

    Question 4

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// test of assisngment
+#include <stdio.h>
+#include "mbed.h"
+
+
+ int A = 10  ;
+    int B = 20 ;
+    int C = 30 ;
+
+
+int main() {
+
+   
+    int D = A + B ;
+
+    int E = C - D ;
+    
+
+printf("D is = %d\n", D);
+printf("E is = %d\n", E);
+    
+    return 0;
+
+}
+
